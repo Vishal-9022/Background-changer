4b4b6e1561025028849655f3c2a646e5def22f01
Vishal_Aher_9022 <vishalaher9022@gmail.com>
2024-02-29 11:08:04 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,151 @@
+var container = document.querySelector("#container");
+var btn1 = document.querySelector("#btn1");
+var btn2 = document.querySelector("#btn2");
+var btn3 = document.querySelector("#btn3");
+var btn4 = document.querySelector("#btn4");
+var btn5 = document.querySelector("#btn5");
+var reset = document.querySelector("#reset button");
+
+btn1.addEventListener("click", function() {
+    container.style.backgroundColor = "skyblue";
+});
+
+btn2.addEventListener("click", function() {
+    container.style.backgroundColor = "orangered";
+});
+
+btn3.addEventListener("click", function() {
+    container.style.backgroundColor = "pink";
+});
+
+btn4.addEventListener("click", function() {
+    container.style.backgroundColor = "violet";
+});
+
+btn5.addEventListener("click", function() {
+    container.style.backgroundColor = "orange";
+});
+
+reset.addEventListener("click", function() {
+    container.style.backgroundColor = "black";
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var color = "";
+
+// var container = document.querySelector("#container")
+// var btn1 = document.querySelector("#btn1")
+// var btn2 = document.querySelector("#btn2")
+// var btn3 = document.querySelector("#btn3")
+// var btn4 = document.querySelector("#btn4")
+// var btn5 = document.querySelector("#btn5")
+// var reset = document.querySelector("#reset")
+
+
+// btn1.addEventListener("click", function(){
+//     container.style.backgroundColor="skyblue";
+   
+
+// })
+
+// btn2.addEventListener("click", function(){
+// //    
+//     container.style.backgroundColor="orangered";
+
+// })
+
+// btn3.addEventListener("click", function(){
+//     container.style.backgroundColor="pink";
+// })
+// btn4.addEventListener("click", function(){
+//     container.style.backgroundColor="violet";
+// })
+
+// btn5.addEventListener("click", function(){
+//     container.style.backgroundColor="orange";
+// })
+
+// reset.addEventListener("click", function(){
+//     container.style.backgroundColor="black";
+// })
+
+
+
+
+// var isstatus = document.querySelector("h5")
+
+// var btn = document.querySelector("#add")
+
+// var check = 0
+
+// btn.addEventListener("click", function(){
+//     if(check == 0){
+//     isstatus.innerHTML = "Friends"
+//     isstatus.style.color = "green"
+//     btn.innerHTML = "Remove Friend"
+//     check = 1
+// }
+// else {
+//     isstatus.innerHTML = "Stranger"
+//     isstatus.style.color = "red"
+//     btn.innerHTML = "Add Friend"
+    
+//     check = 0
+
+// }
+// })
+
